97d19b6de43f7c7b3ef545e195b1e58ac9334c44
ahmedemad <ahmedalabdaly96@gmail.com>
2025-09-28 19:12:42 +0300

    create animation for splash screen

---
+++ lib/pages/splash.dart
@@ -0,0 +1,48 @@
+
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:gap/gap.dart';
+import 'package:project_flutter_one/pages/home.dart';
+import 'package:project_flutter_one/widgets/animation_line.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+
+
+  @override
+  void initState() {
+     Future.delayed(
+        Duration(milliseconds: 800),
+        () {
+          Navigator.pushReplacement(context, MaterialPageRoute(builder: (c) => Home()));
+        }
+    );
+    super.initState();
+  
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      
+      children: [
+       Center(child: SvgPicture.asset("assets/logo/logo.svg"),
+       ),
+       Gap(10),
+
+      
+      AnimationLine()
+
+
+
+    ],),);
+  }
+}
---
+++ lib/widgets/animation_line.dart
@@ -0,0 +1,57 @@
+
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class AnimationLine extends StatefulWidget {
+  const AnimationLine({super.key});
+
+  @override
+  State<AnimationLine> createState() => _AnimationLineState();
+  
+}
+
+class _AnimationLineState extends State<AnimationLine> with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+
+   @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this, // هسه يشتغل طبيعي
+      duration: const Duration(milliseconds: 800),
+    )..forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose(); // مهم حتى ما يسبب memory leak
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return  Directionality(
+      textDirection: TextDirection.rtl,
+      child: AnimatedBuilder(
+        animation: _controller,
+
+        builder: (_, __) {
+          return ShaderMask(
+            shaderCallback: (bounds) {
+              return LinearGradient(
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                stops: [_controller.value , 3.0 , 4.0],
+                colors: [Colors.transparent,Colors.black ,Colors.black],
+              ).createShader(bounds);
+            },
+            blendMode: BlendMode.dstIn,
+            child: SvgPicture.asset('assets/logo/line.svg'),
+          );
+        },
+      ),
+    );;
+  }
+}
